84e650bfcad0ee6d3e7e052895eb33f80b9890af
gr8greengorilla <83386401+gr8greengorilla@users.noreply.github.com>
2022-03-04 07:46:48 -0800

    Updated labeling to work with new model

--- datasetup/labels.py
+++ datasetup/labels.py
@@ -1,30 +1,68 @@
-import os
 import cv2
 import pandas as pd
+import numpy as np
+import mediapipe
 
+mpPose = mediapipe.solutions.pose
+pose = mpPose.Pose()
+def getPose(img):
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = pose.process(imgRGB).pose_landmarks.landmark
+
+    #landmarks = [results[15],results[16],results[30],results[31]] + [results[x] for x in range(23,26)]
+
+    inputs = [[x.x,x.y,x.z] for x in results]
+
+    return inputs
 
 labels = []
+categories = []
 df = pd.read_csv(r"C:\Users\erik\OneDrive\Cross-Code\VSCode\PythonCode\Starting_Code\Machine Learning\pytorchlearning\Speed\datalabels.csv")
 i = 0
-input("Did you change the digit number?")
-while True:
+
+path = df.at[5, "videopath"]
+print(path)
+vidcap = cv2.VideoCapture(path)
+success,image = vidcap.read()
+count = 0
+
+while success:
+    print(count)
+    count += 1
+    #try:
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+    imS = cv2.resize(image, (1500, 1500))
+    cv2.imshow("frame", imS)
+
     try:
-        cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-        fname = "frames/v5frame" + str(i) + ".jpg"
-        print(fname)
-        img = cv2.imread(fname)
-        imS = cv2.resize(img, (1500, 1500))
-        cv2.imshow("frame", imS)
+        landmarkdata = getPose(image)
+        #landmarkdata = [landmarkdata[0], landmarkdata[1], landmarkdata[2]]
 
         k = chr(cv2.waitKey())
         if k == "p":
-            df.loc[len(df)] = [fname, 0]
+            #np.append([getPose(image), 0], labels)
+            labels.append([landmarkdata])
+            categories.append(0)
         elif k == "a":
-            df.loc[len(df)] = [fname, 1]
+            #np.append(labels, np.array([getPose(image), 1],dtype=object))
+            labels.append([landmarkdata])
+            categories.append(1)
+        elif k == "m":
+            break
         
-        i += 1
-    except:
-        break
+        success,image = vidcap.read()
 
-df.to_csv("datalabels2.csv", index=False)
-cv2.destroyAllWindows()
+
+    except:
+        success,image = vidcap.read()
+
+    
+    #except:
+    #    break
+output = np.array(labels)
+categories = np.array(categories)
+print(categories)
+np.save("/".join(path.split("/")[0:-1]) + "/posedata", output)
+np.save("/".join(path.split("/")[0:-1]) + "/categorydata", categories)
